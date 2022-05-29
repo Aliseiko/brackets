790cd8685764d7e46227e1f8e3bf96fbcb20d129
Aliseiko <aliseiko.dev@gmail.com>
2022-05-29 15:36:52 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ module.exports = function check(str, bracketsConfig) {
     let stack = [str[0]];
 
     for (let i = 1; i < str.length; i++) {
-        if (str[i] === matrix[stack[stack.length - 1]]) {
-            stack.pop();
-        } else {
-            stack.push(str[i]);
-        }
+        (str[i] === matrix[stack[stack.length - 1]]) ? stack.pop() : stack.push(str[i]);
     }
 
     return (!stack.length)
